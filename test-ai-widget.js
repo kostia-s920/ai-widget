fcd758045b63be14231f7bb2c72619d5857b1650
Atreus <y.vitchenko@academyocean.com>
2024-09-11 17:18:00 +0300

    update btn disabled

--- test-ai-widget.js
+++ test-ai-widget.js
@@ -232,7 +232,7 @@
         })
 
         inputForm.addEventListener('input', () => {
-            btnGenerate.disabled = inputForm.value.length > 1 ? true : false;
+            btnGenerate.disabled = inputForm.value.length <= 1;
         })
 
         shadow.getElementById('generate-course').addEventListener('click', function() {
